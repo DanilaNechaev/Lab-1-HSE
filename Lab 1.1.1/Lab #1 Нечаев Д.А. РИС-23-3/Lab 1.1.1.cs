3acd03b9b6e35258678b8d8ac8faedabed376750
DanilaNechaev <76594993+DanilaNechaev@users.noreply.github.com>
2023-10-29 22:05:49 +0500

    Add files via upload

---
+++ Lab 1.1.1/Lab #1 Нечаев Д.А. РИС-23-3/Lab 1.1.1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+
+namespace Lab1_1_1 {
+
+    class Programm
+    {
+        static void Main()
+        {
+
+            double m;
+            bool isCorrect;
+            do
+            {
+                Console.WriteLine("Введите  m");
+                string input = Console.ReadLine();
+                isCorrect = double.TryParse(input, out m);
+                if (!isCorrect)
+                {
+                    Console.WriteLine("Введено неподходящее значение");
+                }
+
+            } while (!isCorrect);
+
+            double n;
+            do
+            {
+                Console.WriteLine("Введите n");
+                string input = Console.ReadLine();
+                isCorrect = double.TryParse(input, out n);
+
+                if (!isCorrect)
+                {
+                    Console.WriteLine("Введено неподходящее значение");   
+                }
+            } while(!isCorrect);
+
+            Console.WriteLine($"m = {m} n = {n}");
+            double First = m + --n;
+            Console.WriteLine($"m+--n = {First}");
+
+        }
+    }
+}
---
+++ Lab 1.1.3/Lab 1.1.3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+
+namespace Lab1_1_3
+{
+
+    class Programm
+    {
+        static void Main()
+        {
+
+            double m;
+            bool isCorrect;
+            do
+            {
+                Console.WriteLine("Введите  m");
+                string input = Console.ReadLine();
+                isCorrect = double.TryParse(input, out m);
+                if (!isCorrect)
+                {
+                    Console.WriteLine("Введено неподходящее значение");
+                }
+
+            } while (!isCorrect);
+
+            double n;
+            do
+            {
+                Console.WriteLine("Введите n");
+                string input = Console.ReadLine();
+                isCorrect = double.TryParse(input, out n);
+
+                if (!isCorrect)
+                {
+                    Console.WriteLine("Введено неподходящее значение");
+                }
+            } while (!isCorrect);
+
+            Console.WriteLine($"m = {m} n = {n}");
+            bool Third = --m > n--;
+            if (Third)
+            {
+                Console.WriteLine("--m>n-- - Правда");
+            }
+            else
+            {
+                Console.WriteLine("--m>n-- - Ложь");
+            }
+
+
+        }
+    }
+}
